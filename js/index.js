6bc41f8dc0fc2caf03b21c5e441bc0992b3705b9
jonh CARTER <thadal.winsky@gmail.com>
2023-05-11 22:23:38 +0200

    correction de la navigation, fermer un quand l'autre est ouvert

--- js/index.js
+++ js/index.js
@@ -6,18 +6,33 @@ let localisationIcon = document.getElementById("localisation-icon");
 let adressToggle = document.getElementById("address-toggle");
 
 
+function ifOpen(element) {
+    if (element.classList.contains("display")) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 humbergerIcon.addEventListener("click", (e) => {
     e.preventDefault;
     navigationHeaderUl.classList.toggle('display');
+    
+    if (ifOpen(adressToggle) === true) {
+        console.log("ouvert ")
+        adressToggle.classList.remove("display");
+    }
+    else {
+        console.log("fermer")
+    }
 });
 
 localisationIcon.addEventListener("click", (e) => {
     e.preventDefault;
     adressToggle.classList.toggle('display');
+    if (ifOpen(navigationHeaderUl) === true) {
+        navigationHeaderUl.classList.remove("display");
+    }
+
 });
-
-
-
-
-
-
