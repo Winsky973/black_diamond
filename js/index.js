ddd79a9faa9dfa01a27cc202d3c70320f2457121
jonh CARTER <thadal.winsky@gmail.com>
2023-05-11 23:03:47 +0200

    correction de la navigation, fermer un quand l'autre est ouvert + ajout de la ârtie horaires du retaurant

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 // Navgation mobile 
 let navigationHeaderUl = document.getElementById("navigation-header-ul");
 let humbergerIcon = document.getElementById("menu-toggle");
+let horaireIcon = document.getElementById("horaire-icon");
 
 let localisationIcon = document.getElementById("localisation-icon");
 let adressToggle = document.getElementById("address-toggle");
+let horaireToggle = document.getElementById("horaire-toggle");
 
 
 function ifOpen(element) {
@@ -19,20 +21,26 @@ humbergerIcon.addEventListener("click", (e) => {
     e.preventDefault;
     navigationHeaderUl.classList.toggle('display');
     
-    if (ifOpen(adressToggle) === true) {
-        console.log("ouvert ")
+    if (ifOpen(adressToggle) === true || ifOpen(horaireToggle) === true) {
         adressToggle.classList.remove("display");
-    }
-    else {
-        console.log("fermer")
+        horaireToggle.classList.remove("display");
     }
 });
 
 localisationIcon.addEventListener("click", (e) => {
     e.preventDefault;
     adressToggle.classList.toggle('display');
-    if (ifOpen(navigationHeaderUl) === true) {
+    if (ifOpen(navigationHeaderUl) === true || ifOpen(horaireToggle) === true) {
         navigationHeaderUl.classList.remove("display");
+        horaireToggle.classList.remove("display");
     }
+});
 
+horaireIcon.addEventListener("click", (e) => {
+    e.preventDefault;
+    horaireToggle.classList.toggle('display');
+    if (ifOpen(navigationHeaderUl) === true || ifOpen(adressToggle)) {
+        navigationHeaderUl.classList.remove("display");
+        adressToggle.classList.remove("display");
+    }
 });
